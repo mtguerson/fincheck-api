024215420de397d03cad43bedf5a3aca71db8786
Matheus <mtguerson@gmail.com>
2024-04-01 10:19:02 -0400

    creating my own user repository

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,17 +1,18 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { PrismaService } from 'src/database/prisma.service';
 import { hash } from 'bcryptjs';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly usersRepo: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
     const { name, email, password } = createUserDto;
 
-    const emailTaken = await this.prismaService.user.findUnique({
+    const emailTaken = await this.usersRepo.findUnique({
       where: { email },
+      select: { id: true },
     });
 
     if (emailTaken) {
@@ -20,7 +21,7 @@ export class UsersService {
 
     const hashedPassword = await hash(password, 12);
 
-    const user = await this.prismaService.user.create({
+    const user = await this.usersRepo.create({
       data: {
         name,
         email,
