6144f4882793a611a669927e1e4b919ee8f785e4
Matheus <mtguerson@gmail.com>
2024-04-01 15:09:43 -0400

    wip: auth

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Get('/me')
+  me() {
+    return this.usersService.getUserById('userId');
   }
 }
