ce768968e05a46668b0b1549b3c0729c8d29e8f0
Matheus Guerson <mtguerson@gmail.com>
2024-04-02 11:02:02 -0400

    wip

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin';
 import { SignupDto } from './dto/signup';
@@ -8,12 +8,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
+  @SetMetadata('IS_PUBLIC', true)
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
   }
 
+  @SetMetadata('IS_PUBLIC', true)
   @Post('signup')
-  create(@Body() signupDto: SignupDto) {
+  signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
-  getUserById(userId: string) {
-    return { userId };
+  constructor(private readonly userRepo: UsersRepository) {}
+
+  async getUserById(userId: string) {
+    return this.userRepo.findUnique({
+      where: { id: userId },
+      select: {
+        name: true,
+        email: true,
+      },
+    });
   }
 }
